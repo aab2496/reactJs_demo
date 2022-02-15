e7d0930f172a1a5410828b0fcb635043d412f728
Alatishe, Ayoola <ayoola.alatishe@carefirst.com>
2022-02-15 14:39:42 -0500

    added verbose logs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import PlanSummary from "./containers/plan-summary";
-import { login } from "./services/auth";
 import { getIdCard } from "./services/restClient";
 
 const App = () => {
