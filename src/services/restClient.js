19f06002a9dd466f29cda86b5e1bffd80f30c011
Alatishe, Ayoola <ayoola.alatishe@carefirst.com>
2022-02-23 13:21:42 -0500

    changed harcoded url to deva for test purposes.

--- src/services/restClient.js
+++ src/services/restClient.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
 export const getIdCard = () => {
-    axios({
-        method: 'get',
-        url: 'https://api-pit.carefirst.com/v1/cf-idcards/members/idcards/ID_FRONT-171-917218512-230777001',
-        headers: { 
-            'Access-Control-Allow-Origin': "*",
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJtRHZpZUkzQWlNMDV6Y29hbnlJbCIsImF1ZCI6InN5c3RlbSIsImp0byI6IjlBY3AxN2VRNXFsSlFpMm0yQjh6dFFrbXlSUVEiLCJwcm9maWxlVHlwZSI6InN5c3RlbSIsInByb2ZpbGVJZCI6Ii0xIiwiaXNzIjoiYXBpLXBpdC5jYXJlZmlyc3QuY29tIiwiY2ZhcGlrZXkiOiJtRHZpZUkzQWlNMDV6Y29hbnlJbCIsImV4cCI6MTY0NDk1NTk2MiwiaWF0IjoxNjQ0OTQ4NzYyLCJqdGkiOiJlZDI2MzI4MS00ZTZkLTQ2OWYtOTg2OS02NjFlOGZjMGNjMTcifQ.W-djkLBIsiM0L17P3_SZn-GtBtji6ChPI3B5Qh1OkqoSGBIwtBzs0_Da7g_rPtjNJ2bp9XfDGuKqYlS1MtGDcxESjbSin8bb7EAuW4WiLSxbapZUw4q3VO3vmamA7K_KA5BgVweSGhYwbXcK94Qnd93Urq_dF6O3mqWn8mcLNLjVa-lY7TbQJEgbeFaFPkNhGMx_wJMzz3JmuTXZJsR_YzRs3qS_Ei2tsYds7cgSMtoEIJh7RrBAD82RTmNdmokRhCt6Z4j99yvrxxkOTe20K-HnQcvO_GZBTZ6T9djqKsLRN2R9mO-U1admZidoPPErcr8GtSHEVzWbBmfc8eqbmA'
-        }
-      })
+
+    axios
+      .get('https://api-deva.carefirst.com/v1/cf-idcards/members/idcards/ID_FRONT-171-917218512-230777001',
+       {
+        withCredentials: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ6V3ZuZzNIWTVnUlV5WkplMzhFMSIsImF1ZCI6InN5c3RlbSIsImp0byI6IlRGbGFQYUZkZWJ1YnVHQUFEWlVQeG95NWpTMnMiLCJwcm9maWxlVHlwZSI6InN5c3RlbSIsInByb2ZpbGVJZCI6Ii0xIiwiaXNzIjoiYXBpLWRldmEuY2FyZWZpcnN0LmNvbSIsImNmYXBpa2V5Ijoield2bmczSFk1Z1JVeVpKZTM4RTEiLCJleHAiOjE2NDUyMDU1MjUsImlhdCI6MTY0NTE5ODMyNSwianRpIjoiZDRhMmM4NDQtMmQ4NS00OTlhLTgyZjAtNzc5YjU1YmI0MDAwIn0.SHIksem5lWl1zNT7h2uP0OTyquewY7-0ure9VcXybXR-x3L73KYhC8omANuue0jGm4geGpS05frRu6q1aXVj3a_DPfFj82mU3G0HVyg1OHnCbJJ-owDcIuqrLEts1dA75jHf9o4Q_4bSvhyaPfVOjKQhRNsW8mqBDv41Itvq2sfxD5zuCsnFhgiKJ7BIQ2mcxPGHkJJS16qfHpKtxFT3iHUm4apbNDS_db4ZOOOrEChNKapFb_lemwQoBFGFo6zmxYk5dlGFUjGf3zv6E7y5AHucN7WBv7vKp1qbVXmYijNRkAY7Vgiwc4mlGbBn2plVgCwvxONwViLudQTcX3iFmw'
+          }
+       })
+
+    // axios({
+    //     method: 'get',
+    //     url: 'https://api-deva.carefirst.com/v1/cf-idcards/members/idcards/ID_FRONT-171-917218512-230777001',
+    //     headers: { 
+    //         'Access-Control-Allow-Origin': "*",
+    //         'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ6V3ZuZzNIWTVnUlV5WkplMzhFMSIsImF1ZCI6InN5c3RlbSIsImp0byI6IlRGbGFQYUZkZWJ1YnVHQUFEWlVQeG95NWpTMnMiLCJwcm9maWxlVHlwZSI6InN5c3RlbSIsInByb2ZpbGVJZCI6Ii0xIiwiaXNzIjoiYXBpLWRldmEuY2FyZWZpcnN0LmNvbSIsImNmYXBpa2V5Ijoield2bmczSFk1Z1JVeVpKZTM4RTEiLCJleHAiOjE2NDUyMDU1MjUsImlhdCI6MTY0NTE5ODMyNSwianRpIjoiZDRhMmM4NDQtMmQ4NS00OTlhLTgyZjAtNzc5YjU1YmI0MDAwIn0.SHIksem5lWl1zNT7h2uP0OTyquewY7-0ure9VcXybXR-x3L73KYhC8omANuue0jGm4geGpS05frRu6q1aXVj3a_DPfFj82mU3G0HVyg1OHnCbJJ-owDcIuqrLEts1dA75jHf9o4Q_4bSvhyaPfVOjKQhRNsW8mqBDv41Itvq2sfxD5zuCsnFhgiKJ7BIQ2mcxPGHkJJS16qfHpKtxFT3iHUm4apbNDS_db4ZOOOrEChNKapFb_lemwQoBFGFo6zmxYk5dlGFUjGf3zv6E7y5AHucN7WBv7vKp1qbVXmYijNRkAY7Vgiwc4mlGbBn2plVgCwvxONwViLudQTcX3iFmw'
+    //     }
+    //   })
         .then(function (response) {
             console.log(JSON.stringify(response.data));
         })
